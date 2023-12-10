3d3670b1889b3305e40ab2d31cb3609f8d4ce5a0
James Hook <hempnall@yahoo.co.uk>
2023-12-10 12:26:31 +0000

    day10

--- day10/a.py
+++ day10/a.py
@@ -3,6 +3,10 @@ import sys
 sys.setrecursionlimit(15000)
 f = open("/Users/jameshook/dev/aoc2023/day10/input.txt","r")
 lines = [ l.strip() for l in f.readlines() ]
+LEFTS=set([])
+RIGHTS=set([])
+PATH=[]
+
 
 # | is a vertical pipe connecting north and south.
 # - is a horizontal pipe connecting east and west.
@@ -17,6 +21,14 @@ NORTH=(0,-1)
 SOUTH=(0,1)
 EAST=(1,0)
 WEST=(-1,0)
+
+PIPE_SQUARES=set([])
+PIPE_SQUARES_DIR={
+    NORTH: set([]),
+    EAST: set([]),
+    WEST: set([]),
+    SOUTH: set([])
+}
 
 ALL_DIRS=[NORTH,SOUTH,EAST,WEST]
 
@@ -44,6 +56,21 @@ new_dirs={
     (WEST,"F"):SOUTH
 }
 
+CORNERS={
+    ("F",WEST): [SOUTH,WEST],
+    ("F", NORTH): [EAST,NORTH],
+    ("L",SOUTH):[EAST,SOUTH],
+    ("L",WEST):[NORTH,WEST],
+    ("J",EAST):[NORTH,EAST],
+    ("J",SOUTH):[SOUTH,WEST],
+    ("7",EAST):[EAST,SOUTH],
+    ("7",NORTH):[NORTH, WEST],
+    ("-",EAST):[EAST],
+    ("-",WEST):[WEST],
+    ("|",NORTH): [NORTH],
+    ("|",SOUTH): [SOUTH]
+}
+
 
 OPP_DIRS={
     NORTH: SOUTH,
@@ -51,7 +78,6 @@ OPP_DIRS={
     WEST: EAST,
     SOUTH: NORTH
 }
-
 
 def add_tuple( f , s):
     return (f[0]+s[0],f[1]+s[1])
@@ -72,20 +98,72 @@ def find_first_steps( start_pos ):
     return poss_steps
 
 def step( pos , dir , start, count ):
-    print(count)
     new_coord=add_tuple(pos,dir)
     char_at_adj=lines[new_coord[1]][new_coord[0]]
     if char_at_adj == 'S' and count> 1:
-        print(f'------back {count}')
-        print(count/2)
+        pass
     else:
+        PATH.append(new_coord)
+        PIPE_SQUARES.add(new_coord)
+        ORIENT=CORNERS[(char_at_adj,dir)]
+        for ort in ORIENT:
+            PIPE_SQUARES_DIR[ort].add(new_coord)
+        
         next_dir=new_dirs[(dir,char_at_adj)]    
         step( new_coord ,next_dir ,start, count+1 )
 
 START=find_start(lines)
+PIPE_SQUARES.add(START)
+PATH.append(START)
 get_first_step=find_first_steps(START)
-print(START)
-print(get_first_step)
 step(START, get_first_step[0], START, 1)
-step(START, get_first_step[1], START, 1)
+
+def flood_fill( INSIDE_OUTSIDE , OUTSIDE_INSIDE, start ):
+    if start[0] in [-1, len(lines[0])] or start[1] in [-1, len(lines)]:
+        return
+    if start in INSIDE_OUTSIDE:
+        return
+    if start in OUTSIDE_INSIDE:
+        raise "ooooooooops"
+    if start in PIPE_SQUARES:
+        return
+    INSIDE_OUTSIDE.add(start)
+    for dir in ALL_DIRS:
+        flood_fill( INSIDE_OUTSIDE , OUTSIDE_INSIDE, add_tuple(start,dir))
+
+for idx in range(0, len(PATH)):
+    location=PATH[idx]
+    if location in PIPE_SQUARES_DIR[NORTH]:
+        flood_fill( LEFTS , RIGHTS, add_tuple(location,WEST))
+        flood_fill( RIGHTS , LEFTS , add_tuple(location,EAST))
+    elif location in PIPE_SQUARES_DIR[SOUTH]:
+        flood_fill( RIGHTS , LEFTS, add_tuple(location,WEST))
+        flood_fill( LEFTS , RIGHTS, add_tuple(location,EAST))
+    elif location in PIPE_SQUARES_DIR[EAST]:
+        flood_fill( RIGHTS , LEFTS, add_tuple(location,SOUTH))
+        flood_fill( LEFTS , RIGHTS, add_tuple(location,NORTH))
+    elif location in PIPE_SQUARES_DIR[WEST]:
+        flood_fill( LEFTS , RIGHTS, add_tuple(location,SOUTH))
+        flood_fill( RIGHTS , LEFTS, add_tuple(location,NORTH))
+
+print( len(LEFTS))
+print( len(RIGHTS))
+print( len(PIPE_SQUARES))
+
+print( len(LEFTS) + len(RIGHTS) + len(PIPE_SQUARES))
+print( 140 ** 2)
+
+for row_idx in range(0,len(lines)):
+    for col_idx in range(0,len(lines[row_idx])):
+        if (col_idx,row_idx) in LEFTS:
+            print("L",end = '')
+        elif (col_idx,row_idx) in RIGHTS:
+            print("R",end = '')
+        elif (col_idx,row_idx) in PIPE_SQUARES:
+            print(".",end = '')
+        else:
+            print("*",end = '')
+
+    print()
+
 
